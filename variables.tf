0b8276bc0b9960b4e8396d25cf6ba9287b9b8685
veeranki2014 <veeranki2014@gmail.com>
2023-10-04 13:48:00 -0500

    terraform rabbitmq module

--- variables.tf
+++ variables.tf
@@ -5,3 +5,12 @@ variable "tags" {}
 variable "sg_subnet_cidr" {}
 variable "kms_key_arn" {}
 variable "vpc_id" {}
+variable "engine" {}
+variable "master_username" {}
+variable "master_password" {}
+variable "port" {
+  default = "27017"
+}
+variable "engine_version" {}
+variable "db_instance_count" {}
+variable "instance_class" {}
